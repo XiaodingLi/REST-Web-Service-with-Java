5dd48a52341ef0e4c113b5ba48ea5923d3d638c7
XiaodingLi <lixiaoding07@gmail.com>
2016-08-05 17:33:14 -0400

    Create Response.java

---
+++ src/main/java/com/pojo/Response.java
@@ -0,0 +1,26 @@
+package com.pojo;
+
+import java.util.List;
+
+public class Response {
+
+	private int userCount;
+	private List<Posts> posts;
+
+	public List<Posts> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Posts> posts) {
+		this.posts = posts;
+	}
+
+	public int getUserCount() {
+		return userCount;
+	}
+
+	public void setUserCount(int userCount) {
+		this.userCount = userCount;
+	}
+
+}
